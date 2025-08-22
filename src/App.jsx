a72ce14229f4a6c463b63ddab1aab37d36e16a86
Muhammad Rayyan <i221022@nu.edu.pk>
2025-08-22 23:44:18 +0500

    Fix import path case sensitivity for Vercel deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import Navbar from "./sections/navbar";
+import Navbar from "./sections/Navbar";
 import Hero from "./sections/Hero";
 import Loader from "./components/Loader";
 
